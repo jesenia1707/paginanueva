a322c2e6ccaa07e2d54e04c6288eef28b0655d51
jesenia1707 <jes.pardo@duocuc.cl>
2025-10-13 17:08:05 +0000

    Tu mensaje de commit

---
+++ src/data/Products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: 'Paracetamol',
+    description: 'Descripción breve del producto 1.',
+    price: 1.490,
+    image: 'https://xtremeplay.cl/wp-content/uploads/2023/01/TOYCOLGSM2417.jpg' // Imagen placeholder
+  },
+  {
+    id: 2,
+    name: 'Ibuprofeno',
+    description: 'Descripción breve del producto 2.',
+    price: 2.232,
+    image: 'https://epicforge.cl/cdn/shop/files/furyu_noodle_stopper_cranenking_figure_-_frieren_frieren_beyond_journey_s_end__240919040020.jpg?v=1728077092&width=1024'
+  },
+  {
+    id: 3,
+    name: 'Amoxicilina',
+    description: 'Descripción breve del producto 3.',
+    price: 4.364,
+    image: 'https://media3.nin-nin-game.com/81829/gabriel-dropout-satania-nendoroid-972-.jpg'
+  },
+  {
+    id: 4,
+    name: 'Diclofenaco',
+    description: 'Descripción breve del producto 3.',
+    price: 3.590,
+    image: 'https://media3.nin-nin-game.com/81829/gabriel-dropout-satania-nendoroid-972-.jpg'
+  }
+];
+
+export default products;
